2167172ea08e5ec62a9d0178894cb3cdb6fa3da2
St10457625 <St10457625@vcconect.edu.za>
2025-04-13 10:40:39 +0200

    Created ASCII art and code to play audio greeting

--- PROG_POE_Part1/Program.cs
+++ PROG_POE_Part1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,50 @@ namespace PROG_POE_Part1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+
+            //Play audio greeting
+            PlayGreetingAudio("Cyberbot_greeting.wav");
+
+            Console.Title = "Cyber Security Chatbot";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(@"
+
+ ██████╗██╗   ██╗██████╗ ███████╗██████╗ ██████╗  ██████╗ ████████╗
+██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔═══██╗╚══██╔══╝
+██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝██████╔╝██║   ██║   ██║   
+██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗██╔══██╗██║   ██║   ██║   
+╚██████╗   ██║   ██████╔╝███████╗██║  ██║██████╔╝╚██████╔╝   ██║   
+ ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝   
+                                                                   
+");  //ASCII ART
+
+
+        }
+
+        static void PlayGreetingAudio(string filePath)
+        {
+            try
+            {
+                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+
+                if (File.Exists(fullPath))
+                {
+                    SoundPlayer player = new SoundPlayer(fullPath);
+                    player.PlaySync(); //Play audio synchronousely
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error playing audio: {ex.Message}");
+
+            }
         }
     }
 }
